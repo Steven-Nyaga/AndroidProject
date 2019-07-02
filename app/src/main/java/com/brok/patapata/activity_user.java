d310f67bab218a726cc75fd5cca3d623eb519890
Brok <brianokoth11@gmail.com>
2019-07-02 15:37:04 +0300

    trt

--- app/src/main/java/com/brok/patapata/activity_user.java
+++ app/src/main/java/com/brok/patapata/activity_user.java
@@ -3,6 +3,13 @@ package com.brok.patapata;
 import android.content.Intent;
 import androidx.annotation.NonNull;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -13,10 +20,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class activity_user extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
-
+    DatabaseReference Users;
+    private String user_email;
+    private TextView  cUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +45,24 @@ public class activity_user extends AppCompatActivity implements NavigationView.O
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MainFragmentUser()).commit();
 
+        View header = navigationView.getHeaderView(0);
+        cUser = (TextView) header.findViewById(R.id.currentUser);
+        //cUser = (TextView) findViewById(R.id.currentUser);
+        Users = FirebaseDatabase.getInstance().getReference("users").child(FirebaseAuth.
+                getInstance().getCurrentUser().getUid());
+        Users.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                user_email = dataSnapshot.child("inputEmail").getValue().toString();
+                cUser.setText(user_email);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+
+            }
+        });
 
     }
 
